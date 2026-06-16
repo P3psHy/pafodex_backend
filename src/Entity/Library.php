5d1b2d07697fd49f21ca16e99ee55439ec032890
P3psHy <76128319+P3psHy@users.noreply.github.com>
2026-06-16 10:15:11 +0200

    feat: Ajout table Set

--- src/Entity/Library.php
+++ src/Entity/Library.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\LibraryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: LibraryRepository::class)]
@@ -16,6 +18,17 @@ class Library
     #[ORM\OneToOne(inversedBy: 'library', cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
     private User $user;
+
+    /**
+     * @var Collection<int, Set>
+     */
+    #[ORM\OneToMany(targetEntity: Set::class, mappedBy: 'library', orphanRemoval: true)]
+    private Collection $sets;
+
+    public function __construct()
+    {
+        $this->sets = new ArrayCollection();
+    }
 
 
     public function getId(): ?int
@@ -31,6 +44,36 @@ class Library
     public function setUser(User $user): static
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Set>
+     */
+    public function getSets(): Collection
+    {
+        return $this->sets;
+    }
+
+    public function addSet(Set $set): static
+    {
+        if (!$this->sets->contains($set)) {
+            $this->sets->add($set);
+            $set->setLibrary($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSet(Set $set): static
+    {
+        if ($this->sets->removeElement($set)) {
+            // set the owning side to null (unless already changed)
+            if ($set->getLibrary() === $this) {
+                $set->setLibrary(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Set.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SetRepository::class)]
+class Set
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'sets')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Library $library = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLibrary(): ?Library
+    {
+        return $this->library;
+    }
+
+    public function setLibrary(?Library $library): static
+    {
+        $this->library = $library;
+
+        return $this;
+    }
+}
